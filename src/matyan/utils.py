5de6c692367ae40453aa5d3186db4a04a3c020b2
Artur Barseghyan <artur.barseghyan@gmail.com>
2020-01-05 23:54:05 +0100

    #14 Find correct release date

--- src/matyan/utils.py
+++ src/matyan/utils.py
@@ -139,9 +139,10 @@ def get_logs(between: str = None, path: str = None) -> Dict[str, Any]:
     tags_with_dates = tags_with_dates_text.split('\n')
     date_tags = {}
     for _log_data_raw in tags_with_dates:
-        _tag, _date = _log_data_raw.split(',')
+        _tag, _date = _log_data_raw.split(',', maxsplit=1)
         _tag = _tag.strip().replace('(tag: ', '').replace(')', '')
-        date_tags.update({_tag: _date})
+        if _tag:
+            date_tags.update({_tag: _date})
 
     text_log_tags_args = []
     # if lower and upper:
@@ -157,6 +158,7 @@ def get_logs(between: str = None, path: str = None) -> Dict[str, Any]:
     log_tags = text_log_tags.split("\n")
     commit_tags_list = [s.split(' ', 1)[0].split('\t', 1) for s in log_tags]
     commit_tags = dict([l for l in commit_tags_list if len(l) > 1])
+
     return {
         'TEXT_LOG_MERGES': text_log_merges,
         'LOG_MERGES': log_merges,
@@ -538,6 +540,7 @@ def prepare_releases_changelog(
                     'description': branch_description,
                     'commits': {},
                     'release': release,
+                    # 'release_date': logs['DATE_TAGS'].get(release, None),
                 }
             branch_types.update({ticket_number: branch_type})
     
